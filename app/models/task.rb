ead73f9cfca65da5a92354f5173f51f76fd6aece
shinsuke <shinst710@gmail.com>
2020-03-28 14:42:03 +0900

    ransack-introduce

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,12 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   belongs_to :user
   scope :recent, -> { order(created_at: :desc) }
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
